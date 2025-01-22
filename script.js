8ee111ac715735359de50fb38c21e457549c980c
Nicola Vettone <nicolavettone57@gmail.com>
2025-01-22 14:42:04 +0100

    libreria fatta

--- script.js
+++ script.js
@@ -9,7 +9,23 @@ fetch("https://striveschool-api.herokuapp.com/books")
   .then((libreriaArr) => {
     console.log(libreriaArr);
 
+    let libreria = document.querySelector("#books");
+
     libreriaArr.forEach((books) => {
-      //console.log(books.price);
+      console.log(books);
+
+      const card = document.createElement("div");
+      card.classList.add("col");
+
+      card.innerHTML = `<div class="card"">
+  <img src="${books.img}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${books.title}</h5>
+    <p class="card-text">${books.price}</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>`;
+      libreria.appendChild(card);
     });
-  });
+  })
+  .catch((err) => console.log(err));
